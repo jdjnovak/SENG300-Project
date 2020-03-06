b5a055354766e54343d9d315ab247fe43f99739b
mudRucker <themudrucker@gmail.com>
2020-03-05 20:36:19 -0700

    Cody: added the file-upload-piece (in 'components' dir)

---
+++ components/file-upload-piece/scripts/controllers.js
@@ -0,0 +1,44 @@
+function UploadCtrl($scope, $firebaseStorage, $firebaseObject) {
+
+    let fileToUpload = null;
+    $scope.onChange = function onChange(fileList) {
+        fileToUpload = fileList[0];
+    };
+    $scope.upload = function() {
+        if (fileToUpload) {
+            let storageRef = firebase.storage().ref(fileToUpload.name);
+            let storage = $firebaseStorage(storageRef);
+            let uploadTask = storage.$put(fileToUpload);
+            uploadTask.$complete((snapshot) => {
+                let ref = firebase.database().ref("Files");
+                let pushKey = ref.push().key;
+                let formData = $firebaseObject(ref.child(pushKey));
+                formData.name = fileToUpload.name;
+                formData.timestamp = firebase.database.ServerValue.TIMESTAMP;
+                formData.url = snapshot.downloadURL;
+                formData.$save().then(() => {
+                    angular.element("input[type='file']").val(null);
+                    fileToUpload = null;
+                })
+            });
+        }
+    }
+}
+
+function tableCtrl($scope, $firebaseStorage, $firebaseObject){
+    let fileRef = firebase.database().ref('Files');
+    $scope.files = $firebaseObject(fileRef);
+    $scope.delete = (key, name) => {
+        let storageRef = firebase.storage().ref(name);
+        let storage = $firebaseStorage(storageRef);
+        storage.$delete().then(() => {
+            delete $scope.files[key];
+            $scope.files.$save();
+        })
+    }
+}
+
+angular
+    .module('app')
+    .controller('UploadCtrl', UploadCtrl)
+    .controller('tableCtrl', tableCtrl)
